8a56486c6f0a2950ed3851a3c96f636717f12a91
Bartosz Kozielski <bartoszkozielski1998@gmail.com>
2024-01-10 05:16:16 +0100

    commit

--- script.js
+++ script.js
@@ -2,28 +2,43 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
   spaceBetween: 0,
   loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false, // Kontynuuj autoplay po interakcji użytkownika
+  },
+  speed: 600,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     renderBullet: function (index, className) {
       if (index < 4) {
-        
         return '<span class="' + className + '"></span>';
       } else {
-       
-        return '';
+        return "";
       }
-    }
+    },
   },
-  
-   breakpoints: {
+
+  breakpoints: {
     0: {
       slidesPerView: 1,
     },
-    768: {
+    320: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    640: {
       slidesPerView: 2.5,
     },
-    1201: {
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 3.5,
+    },
+    1200: {
       slidesPerView: 4,
     },
   },
